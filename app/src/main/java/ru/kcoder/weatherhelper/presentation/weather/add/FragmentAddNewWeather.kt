95f47f8e775639a0e2629589fd09e31abf57ae9a
Kirill Sokolov <sokolov.k.o@yandex.ru>
2018-12-15 20:28:44 +0300

    try to load photos from google pictures

--- app/src/main/java/ru/kcoder/weatherhelper/presentation/weather/add/FragmentAddNewWeather.kt
+++ app/src/main/java/ru/kcoder/weatherhelper/presentation/weather/add/FragmentAddNewWeather.kt
@@ -2,10 +2,12 @@ package ru.kcoder.weatherhelper.presentation.weather.add
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.annotation.NonNull
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.places.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import ru.kcoder.weatherhelper.presentation.common.BaseFragment
@@ -14,16 +16,19 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment
-import com.google.android.gms.location.places.Place
 import com.google.android.gms.location.places.ui.PlaceSelectionListener
+import kotlinx.android.synthetic.main.weather_add_fragment.*
 import ru.kcoder.weatherhelper.toolkit.debug.log
 import ru.kcoder.weatherhelper.toolkit.utils.Permissions
+
+
 
 
 class FragmentAddNewWeather : BaseFragment() {
 
     private var map: GoogleMap? = null
     private var mapView: MapView? = null
+    private var geoData: GeoDataClient? = null
 
 
     override fun onCreateView(
@@ -32,9 +37,45 @@ class FragmentAddNewWeather : BaseFragment() {
     ): View? {
         val view = inflater.inflate(R.layout.weather_add_fragment, container, false)
 
+        activity?.let {
+            geoData = Places.getGeoDataClient(it)
+        }
+
         initMap(view, savedInstanceState)
         initPlace()
         return view
+    }
+
+    @SuppressLint("MissingPermission") //"smart cast" not work
+    private fun initMap(view: View, savedInstanceState: Bundle?) {
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
+        }
+        mapView = view.findViewById(R.id.mapView) as MapView
+        mapView?.onCreate(mapViewBundle)
+        mapView?.getMapAsync { mMap ->
+            map = mMap
+
+            map?.let { gMap ->
+                gMap.uiSettings?.isZoomControlsEnabled = true
+                gMap.uiSettings?.isCompassEnabled = true
+                activity?.let {
+                    if (Permissions.checkLocation(it)) {
+                        gMap.isMyLocationEnabled = true
+                    }
+                }
+
+                gMap.setOnMapLongClickListener {
+                    gMap.clear()
+                    gMap.addMarker(MarkerOptions().position(it))
+            }
+
+                // marker click
+                gMap.setOnInfoWindowClickListener {
+                }
+            }
+        }
     }
 
     private fun initPlace() {
@@ -59,7 +100,7 @@ class FragmentAddNewWeather : BaseFragment() {
                     val cameraPosition = CameraPosition.Builder().target(place.latLng).zoom(10f).build()
                     it.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
                 }
-                log(place.name.toString())
+                getPhoto(place.id)
             }
 
             override fun onError(status: Status) {
@@ -68,27 +109,31 @@ class FragmentAddNewWeather : BaseFragment() {
         });
     }
 
-    @SuppressLint("MissingPermission") //"smart cast" not work
-    private fun initMap(view: View, savedInstanceState: Bundle?) {
-        var mapViewBundle: Bundle? = null
-        if (savedInstanceState != null) {
-            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
-        }
-        mapView = view.findViewById(R.id.mapView) as MapView
-        mapView?.onCreate(mapViewBundle)
-        mapView?.getMapAsync { mMap ->
-            map = mMap
-
-            map?.uiSettings?.isZoomControlsEnabled = true
-            map?.uiSettings?.isCompassEnabled = true
-            activity?.let {
-                if (Permissions.checkLocation(it)) {
-                    map?.isMyLocationEnabled = true
+    fun getPhoto(placeId: String){
+        geoData?.let {gd ->
+            val photoMetadataResponse = gd.getPlacePhotos(placeId)
+            photoMetadataResponse.addOnCompleteListener{t ->
+                val photos = t.result
+                // Get the PlacePhotoMetadataBuffer (metadata for all of the photos).
+                val photoMetadataBuffer = photos?.photoMetadata
+                // Get the first photo in the list.
+                val photoMetadata = photoMetadataBuffer?.get(2)
+                // Get the attribution text.
+                val attribution = photoMetadata?.getAttributions()
+                // Get a full-size bitmap for the photo.
+                val photoResponse = gd.getPhoto(photoMetadata!!)
+                photoResponse.addOnCompleteListener{
+                    val photo = it.result
+                    val bitmap = photo?.bitmap
+                    mapView!!.visibility = View.GONE
+                    frameLayoutPlaceContainer.visibility = View.GONE
+                    ivv.visibility = View.VISIBLE
+                    ivv.setImageBitmap(bitmap)
                 }
             }
         }
-    }
 
+    }
 
     override fun onResume() {
         mapView?.onResume()
